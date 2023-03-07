cc33a99dd0af47751c0ea74ce2b2bb50f77b4a7f
Özgür Atıcı <35876648+aticio@users.noreply.github.com>
2023-03-07 12:18:32 +0000

    main class created

---
+++ pb-renko.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+class Renko:
+    """Renko initialization class
+    """
+    def __init__(self, percent, data):
+        self.percent = percent
+        self.data = data
+        print(self.percent)
