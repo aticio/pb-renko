6ace848b8e5bf62ed53a65abfd8a6ffcd9583bfe
Özgür Atıcı <35876648+aticio@users.noreply.github.com>
2023-03-08 14:09:10 +0000

    chart drawing feature implemented

--- pbrenko.py
+++ pbrenko.py
@@ -1,5 +1,8 @@
 # flake8: noqa
 import math
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+import numpy as np
 
 class PbRenko:
     """Renko initialization class
@@ -13,7 +16,7 @@ class PbRenko:
     
     def create_pbrenko(self):
         gap = float(self.data[0]) * self.percent / 100
-        print("gap:", gap, "d:", self.data[0])
+        # print("gap:", gap, "d:", self.data[0])
         for i, d in enumerate(self.data):
             if i == 0:
                 if len(self.bricks) == 0:
@@ -48,8 +51,8 @@ class PbRenko:
                         else:
                             if self.low_wick == 0 or d < self.low_wick:
                                 self.low_wick = d
-                    print("gap:", gap, "d:", d)
-                    print(self.bricks)
+                    # print("gap:", gap, "d:", d)
+                    # print(self.bricks)
                 elif self.bricks[-1]["type"] == "down":
                     if d < self.bricks[-1]["close"]:
                         delta = self.bricks[-1]["close"] - d
@@ -79,8 +82,8 @@ class PbRenko:
                         else:
                             if d > self.high_wick:
                                 self.high_wick = d
-                    print(self.bricks)
-                    print("gap:", gap, "d:", d)
+                    # print(self.bricks)
+                    # print("gap:", gap, "d:", d)
                 else:
                     if d > self.bricks[-1]["close"]:
                         delta = d - self.bricks[-1]["close"]
@@ -94,8 +97,8 @@ class PbRenko:
                         if fcount != 0:
                             self.add_bricks("down", fcount, gap)
                             gap = d * self.percent / 100
-                    print(self.bricks)
-                    print("gap:", gap, "d:", d)
+                    # print(self.bricks)
+                    # print("gap:", gap, "d:", d)
 
 
     def add_bricks(self, type, count, brick_size, wick=0):
@@ -142,3 +145,53 @@ class PbRenko:
                             self.bricks.append({"type": type, "open": self.bricks[-1]["close"], "close": (self.bricks[-1]["close"] - brick_size), "high": wick})
                         else:
                             self.bricks.append({"type": type, "open": self.bricks[-1]["close"], "close": (self.bricks[-1]["close"] - brick_size)})
+
+
+    def draw_chart(self):
+        min_val = self.bricks[0]["close"]
+        largest_brick_size = 0
+        for b in self.bricks:
+            brick_size = abs(b["close"] - b["open"])
+            if brick_size > largest_brick_size:
+                largest_brick_size = brick_size
+            
+            if b["close"] < min_val:
+                min_val = b["close"]
+
+        brick_width = largest_brick_size / 2
+        y_max = 0
+        fig, ax = plt.subplots()
+
+        count = 1
+        for b in self.bricks:
+            y = 0
+            color = ""
+            if b["type"] == "up":
+                y = b["open"]
+                color = "green"
+            elif b["type"] == "down":
+                y = b["close"]
+                color = "red"
+            else:
+                color = "gray"
+                y = b["close"]
+
+            if y > y_max:
+                y_max = y
+
+            brick_size = (b["close"] * self.percent / 100)
+            r = Rectangle((count * brick_width, y), brick_width, brick_size)
+            r.set_color(color)
+
+            ax.add_patch(r)
+            count = count + 1
+
+        ax.set_xlim(0, count * brick_width)
+        ax.set_ylim(min_val - brick_width, y_max + (y_max * 0.1))
+        ax.set_axisbelow(True)
+        ax.get_xaxis().set_visible(False)
+
+        ticks = np.arange(min_val - brick_width, y_max + (y_max * 0.1), brick_width * 2)
+        plt.yticks(ticks)
+        plt.grid(linestyle='--', color="#ccd8c0")
+        plt.savefig("chart.png")
---
+++ tests/test_drawpbrenko.py
@@ -0,0 +1,19 @@
+from pbrenko import PbRenko
+import requests
+
+BINANCE_URL = "https://api.binance.com/api/v3/klines"
+SYMBOL = "BTCUSDT"
+INTERVAL = "1d",
+LIMIT = 90
+PARAMS = {"symbol": SYMBOL, "interval": INTERVAL, "limit": LIMIT}
+
+
+def test_drawpbrenko():
+    response = requests.get(url=BINANCE_URL, params=PARAMS)
+    data = response.json()
+    close = [float(c[4]) for c in data]
+
+    pbrnk = PbRenko(1, close)
+    pbrnk.create_pbrenko()
+
+    pbrnk.draw_chart()
